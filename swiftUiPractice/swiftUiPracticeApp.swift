00813977167762a0235149b7bbefc36e4023a805
AbGhost-cyber <xplendo@gmail.com>
2022-12-18 17:39:56 +0800

    Initial Commit

---
+++ swiftUiPractice/swiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiPracticeApp.swift
+//  swiftUiPractice
+//
+//  Created by dremobaba on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
