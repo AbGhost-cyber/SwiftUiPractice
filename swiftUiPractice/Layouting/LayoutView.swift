3f3248958652dd63b0b8359b4802f9988d1e5330
AbGhost-cyber <xplendo@gmail.com>
2023-01-19 00:01:21 +0800

    learning layouts aligning

---
+++ swiftUiPractice/Layouting/LayoutView.swift
@@ -0,0 +1,53 @@
+//
+//  LayoutView.swift
+//  swiftUiPractice
+//
+//  Created by dremobaba on 2023/1/18.
+//
+
+import SwiftUI
+struct HalfCard: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "crown.fill")
+                .font(.system(size: 80))
+        }
+        .overlay(alignment: .topLeading) {
+            VStack {
+                Image(systemName: "crown.fill")
+                    .font(.body)
+                Text("Q")
+                    .font(.largeTitle)
+                Image(systemName: "heart.fill")
+                    .font(.title)
+            }
+            .padding()
+        }
+        .border(.brown)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .border(.green)
+    }
+}
+
+struct LayoutView: View {
+    var body: some View {
+          VStack {
+              HalfCard()
+              HalfCard()
+                  .rotationEffect(.degrees(180))
+          }
+          .background(
+              RoundedRectangle(cornerRadius: 10)
+                  .stroke(Color.black)
+          )
+          .aspectRatio(0.70, contentMode: .fit)
+          .foregroundColor(.red)
+          .padding()
+      }
+}
+
+struct LayoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        LayoutView()
+    }
+}
